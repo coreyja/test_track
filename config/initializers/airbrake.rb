c569e166bdb28d15a301a8ef96b9a10fe77f1c6f
Corey Alexander <corey@betterment.com>
2018-05-25 15:48:03 -0400

    Upgrade Airbrake and it's config

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,20 +1,9 @@
 if ENV['AIRBRAKE_API_KEY'].present?
   Airbrake.configure do |config|
-    config.api_key = ENV['AIRBRAKE_API_KEY']
-    config.host    = ENV['AIRBRAKE_HOST'] if ENV['AIRBRAKE_HOST'].present?
-    config.port    = ENV['AIRBRAKE_PORT'].to_i if ENV['AIRBRAKE_PORT'].present?
-    config.secure  = config.port == 443
+    config.project_id = 1 # required, but any positive integer works
+    config.project_key = ENV['AIRBRAKE_API_KEY']
+    config.host = ENV.fetch('AIRBRAKE_URL')
+    config.environment = Rails.env
+    config.ignore_environments = %w(development test)
   end
 end
-
-module RailsFiveAirbrakeWorkaround
-  private
-
-  # Monkey-patching to work around usage of `ActionController::Parameters#to_hash`
-  # https://github.com/airbrake/airbrake/blob/v4.3.8/lib/airbrake/rails/controller_methods.rb#L21
-  def to_hash(params)
-    params.to_unsafe_hash
-  end
-end
-
-ActionController::Base.prepend RailsFiveAirbrakeWorkaround
