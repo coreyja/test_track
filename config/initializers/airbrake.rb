a7351cca9014aa768506f9247aeb96729c3c79fe
Corey Alexander <corey@betterment.com>
2018-05-25 16:01:45 -0400

    Only set the URL if the env var is set, to mirror the old behavior

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -2,7 +2,7 @@ if ENV['AIRBRAKE_API_KEY'].present?
   Airbrake.configure do |config|
     config.project_id = 1 # required, but any positive integer works
     config.project_key = ENV['AIRBRAKE_API_KEY']
-    config.host = ENV.fetch('AIRBRAKE_URL')
+    config.host = ENV['AIRBRAKE_URL'] if ENV['AIRBRAKE_URL'].present?
     config.environment = Rails.env
     config.ignore_environments = %w(development test)
   end
